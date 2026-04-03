6dd5614363c5c472a625aa188e1da566e8464ac8
AbiiRRR-444 <112918813+AbiiRRR-444@users.noreply.github.com>
2026-04-03 21:43:45 +0600

    Update supabase.js
    fix supabase import

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,4 +1,4 @@
-import { createClient } from "https://esm.sh/@supabase/supabase-js";
+import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = "https://kccjggtvbigyakzumphc.supabase.co";
 const supabaseKey = "sb_publishable_XiOJXWoFg-IM2y_bq95I7g_wMSh7OKU";
